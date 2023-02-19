aac90ba43670b3c7bfde2a1dfd6c5fd45e487ff2
Wes T <85148144+stubborngoat82@users.noreply.github.com>
2023-02-18 23:54:56 -0700

    commit changes to main

--- script.js
+++ script.js
@@ -1,18 +1,19 @@
-// Get the image element
+// Get the image and endpoint elements
 var image = document.getElementById("image");
 var endpoint = document.getElementById("endpoint");
 
 // Set the initial position of the image
-var imageLeft = 0;
-var imageTop = 0;
+var imageLeft = Math.round(Math.random() * 450/10) * 10;
+var imageTop = Math.round(Math.random() * 450 / 10)* 10;
 image.style.left = imageLeft + "px";
 image.style.top = imageTop + "px";
 
-// Set a random position for the endpoint
-var endpointLeft = 360;
-var endpointTop = 680;
+// Generate a random position for the endpoint rounded to the nearest 10 pixels
+var endpointLeft = Math.round(Math.random() * 450 / 10) * 10; // between 0 and 450
+var endpointTop = Math.round(Math.random() * 450 / 10) * 10; // between 0 and 450
 endpoint.style.left = endpointLeft + "px";
 endpoint.style.top = endpointTop + "px";
+
 
 // Add event listener to the document to listen for key presses
 document.addEventListener("keydown", function(event) {
@@ -22,23 +23,26 @@ document.addEventListener("keydown", function(event) {
 	// Move the image depending on which arrow key was pressed
 	if (keyCode === 37) { // left arrow
 		imageLeft -= 10;
-		image.style.left = imageLeft + "px";
 	} else if (keyCode === 38) { // up arrow
 		imageTop -= 10;
-		image.style.top = imageTop + "px";
 	} else if (keyCode === 39) { // right arrow
 		imageLeft += 10;
-		image.style.left = imageLeft + "px";
 	} else if (keyCode === 40) { // down arrow
 		imageTop += 10;
-		image.style.top = imageTop + "px";
 	}
+
 	// Check if the image has reached the endpoint
 	if (imageLeft === endpointLeft && imageTop === endpointTop) {
 		alert("You win!");
+		imageTop = Math.round(Math.random() * 450 / 10) * 10;  
+		imageLeft= Math.round(Math.random() * 450 / 10) * 10;  
+		endpointLeft= Math.round(Math.random() * 450 / 10) * 10;  
+		endpointTop= Math.round(Math.random() * 450 / 10) * 10;
 	}
 
 	// Update the position of the image
 	image.style.left = imageLeft + "px";
 	image.style.top = imageTop + "px";
+	endpoint.style.left = endpointLeft + "px";
+	endpoint.style.top = endpointTop + "px";
 });
